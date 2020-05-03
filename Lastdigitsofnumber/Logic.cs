fff27f07fcc67a7a3a59594fffe65dec662a89af
Nikkza <isaksson.nicklas80@outlook.com>
2020-05-03 13:56:07 +0300

    Add project files.

---
+++ Lastdigitsofnumber/Logic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Lastdigitsofnumber
+{
+    public class Logic
+    {
+        public char[] LastDigitsOfaNumber(BigInteger number, long lastdigits)
+        {
+            char[] numberAsCharArray;
+            if (lastdigits <= 0) return new char[] { };
+            else if (number.ToString().Length > lastdigits)
+            {
+                numberAsCharArray = number.ToString()
+                .Substring(number.ToString()
+                .Length - (int)lastdigits).ToCharArray();
+            }
+            else
+                return new char[] { };
+
+            return numberAsCharArray;
+        }
+
+        public List<BigInteger> GetNumberDigiAsList(string value)
+        {
+            BigInteger parsedValue;
+            List<BigInteger> listBigInt = new List<BigInteger>();
+            var split = value.Split(',');
+            foreach (var item in split)
+            {
+                parsedValue = BigInteger.Parse(item);
+                listBigInt.Add(parsedValue);
+            }
+
+            return listBigInt;
+        }
+    }
+}
---
+++ Lastdigitsofnumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Lastdigitsofnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+            while (true)
+            {
+                string numberAndDigits = Console.ReadLine();
+                var getNummbersAsList = logic.GetNumberDigiAsList(numberAndDigits);
+                if (getNummbersAsList.Count > 0)
+                {
+                    var results = logic.LastDigitsOfaNumber(getNummbersAsList[0], (long)getNummbersAsList[1]);
+                    if (!(results.Length <= 0))
+                        Console.WriteLine($"{string.Join(",", results)}");
+                    else
+                        Console.WriteLine($"null {0}");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
